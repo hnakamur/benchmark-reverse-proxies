7c6ac56787a27507cce3cb3e9c354610a6da6ffb
Hiroaki Nakamura <hnakamur@gmail.com>
2024-03-24 22:48:41 +0900

    Use connection pool with hyper-util Client in proxy-hyper

--- proxy-hyper/src/main.rs
+++ proxy-hyper/src/main.rs
@@ -1,28 +1,24 @@
 use std::net::SocketAddr;
 
-use http::{Request, Response, Uri};
+use http::{Request as HttpRequest, Response as HttpResponse, Uri};
 use hyper::body::Incoming;
-use hyper::client::conn;
 use hyper::header::{HeaderValue, SERVER};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
+use hyper_util::client::legacy::{connect::HttpConnector, Client};
 use hyper_util::rt::TokioIo;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpListener;
+
+static CLIENT: Lazy<Client> =
+    Lazy::new(|| Client::builder(hyper_util::rt::TokioExecutor::new()).build(HttpConnector::new()));
 
 async fn hello(
-    req: Request<Incoming>,
-) -> Result<Response<Incoming>, Box<dyn std::error::Error + Send + Sync>> {
+    req: HttpRequest<Incoming>,
+) -> Result<HttpResponse<Incoming>, Box<dyn std::error::Error + Send + Sync>> {
     let mut req = req;
     *req.uri_mut() = "http://localhost:3000".parse::<Uri>().unwrap();
-    let stream = TcpStream::connect(req.uri().authority().unwrap().as_str()).await?;
-    let io = TokioIo::new(stream);
-    let (mut sender, conn) = conn::http1::handshake(io).await?;
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            println!("Connection failed: {:?}", err);
-        }
-    });
-    let mut res = sender.send_request(req).await?;
+    let client = CLIENT.clone();
+    let mut res = client.request(req).await?;
     let headers = res.headers_mut();
     headers.insert(SERVER, HeaderValue::from_static("hyper"));
     Ok(res)
