d8950200528eedf5f7de4aa9245a85d8a4b01f7f
Hiroaki Nakamura <hnakamur@gmail.com>
2024-03-25 21:52:37 +0900

    Update origin-ntex
    Based on https://github.com/TechEmpower/FrameworkBenchmarks/blob/46273dbfa2e9f5e31332682846212c91dede5642/frameworks/Rust/ntex/src/main.rs

--- origin-ntex/src/main.rs
+++ origin-ntex/src/main.rs
@@ -2,11 +2,12 @@ use std::io;
 
 use ntex::http::header::{HeaderValue, CONTENT_TYPE, SERVER};
 use ntex::http::{self, HttpService, Response};
-use ntex::util::PoolId;
+use ntex::util::{Bytes, PoolId};
 use ntex::{time::Seconds, util::Ready};
 
-static TEXT_PLAIN: HeaderValue = HeaderValue::from_static("text/plain");
-static NTEX: HeaderValue = HeaderValue::from_static("ntex");
+const TEXT_PLAIN: HeaderValue = HeaderValue::from_static("text/plain");
+const NTEX: HeaderValue = HeaderValue::from_static("ntex");
+const BODY_TEXT: Bytes = Bytes::from_static(b"Hello, world!\n");
 
 #[ntex::main]
 async fn main() -> io::Result<()> {
@@ -24,12 +25,15 @@ async fn main() -> io::Result<()> {
                 .client_timeout(Seconds::ZERO)
                 .headers_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
                 .payload_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
-                .disconnect_timeout(Seconds(1))
                 .finish(|_req| {
-                    let mut res = Response::Ok();
-                    res.header(CONTENT_TYPE, &TEXT_PLAIN);
-                    res.header(SERVER, &NTEX);
-                    Ready::Ok::<_, io::Error>(res.body("Hello, world!\n"))
+                    let mut res = Response::with_body(
+                        http::StatusCode::OK,
+                        http::body::Body::Bytes(BODY_TEXT),
+                    );
+                    let headers = res.headers_mut();
+                    headers.insert(CONTENT_TYPE, TEXT_PLAIN);
+                    headers.insert(SERVER, NTEX);
+                    Ready::Ok::<_, io::Error>(res)
                 })
         })?
         .run()
