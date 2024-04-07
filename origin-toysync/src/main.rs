750bf972524871c8e24bef618cfbd7056449fb07
Hiroaki Nakamura <hnakamur@gmail.com>
2024-04-07 22:23:38 +0900

    Use multithread in origin-toysync
    https://doc.rust-lang.org/book/ch20-02-multithreaded.html

--- origin-toysync/src/main.rs
+++ origin-toysync/src/main.rs
@@ -1,14 +1,20 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
 use std::time::SystemTime;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    let count = thread::available_parallelism().unwrap().get();
+    let pool = ThreadPool::new(count);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -26,4 +32,64 @@ fn handle_connection(mut stream: TcpStream) {
     let response = format!("HTTP/1.1 200 OK\r\nServer: toysync\r\nDate: {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", date, content.len(), content);
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            // println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
 }
