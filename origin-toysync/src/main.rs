91fe8c12590a14b608c7bb9a31e023e10cddbb6a
Hiroaki Nakamura <hnakamur@gmail.com>
2024-04-14 08:45:13 +0900

    Call accept in multithread in origin-toysync

--- origin-toysync/src/main.rs
+++ origin-toysync/src/main.rs
@@ -1,95 +1,44 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 mod date;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    let listener = Arc::new(Mutex::new(listener));
     let count = thread::available_parallelism().unwrap().get();
-    let pool = ThreadPool::new(count);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    let mut handles = vec![];
 
-        pool.execute(|| {
-            handle_connection(stream);
+    for _ in 0..count {
+        let listener_clone = Arc::clone(&listener);
+        let handle = thread::spawn(move || loop {
+            let listener = listener_clone.lock().unwrap();
+            match listener.accept() {
+                Ok((stream, _addr)) => {
+                    handle_connection(stream);
+                }
+                Err(e) => {
+                    eprintln!("Failed to accept client: {}", e);
+                    break;
+                }
+            }
         });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buf_reader = BufReader::new(&mut stream);
     let _buffer = buf_reader.fill_buf().unwrap();
-    // let _http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
 
     let content = "Hello, world!\n";
     let response = format!("HTTP/1.1 200 OK\r\nServer: toysync\r\nDate: {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", date::now(), content.len(), content);
-
     stream.write_all(response.as_bytes()).unwrap();
-}
-
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
-}
-
-impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-
-        let (sender, receiver) = mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        let mut workers = Vec::with_capacity(size);
-
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        ThreadPool { workers, sender }
-    }
-
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-
-        self.sender.send(job).unwrap();
-    }
-}
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-struct Worker {
-    id: usize,
-    thread: thread::JoinHandle<()>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-
-            // println!("Worker {id} got a job; executing.");
-
-            job();
-        });
-
-        Worker { id, thread }
-    }
 }
