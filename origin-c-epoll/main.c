687f126ebbedd27c6c660013526fb0371f9774a1
Hiroaki Nakamura <hnakamur@gmail.com>
2024-04-14 22:39:00 +0900

    Close client connection for no keepalive in origin-c-epoll

--- origin-c-epoll/main.c
+++ origin-c-epoll/main.c
@@ -16,12 +16,82 @@
 #define THREAD_POOL_SIZE 24
 #define RESPONSE_BODY "Hello, world!\n"
 
+typedef int ngx_int_t;
+typedef unsigned int ngx_uint_t;
+typedef unsigned char u_char;
+
+ngx_int_t
+ngx_strncasecmp(u_char *s1, u_char *s2, size_t n)
+{
+    ngx_uint_t  c1, c2;
+
+    while (n) {
+        c1 = (ngx_uint_t) *s1++;
+        c2 = (ngx_uint_t) *s2++;
+
+        c1 = (c1 >= 'A' && c1 <= 'Z') ? (c1 | 0x20) : c1;
+        c2 = (c2 >= 'A' && c2 <= 'Z') ? (c2 | 0x20) : c2;
+
+        if (c1 == c2) {
+
+            if (c1) {
+                n--;
+                continue;
+            }
+
+            return 0;
+        }
+
+        return c1 - c2;
+    }
+
+    return 0;
+}
+
+/*
+ * ngx_strlcasestrn() is intended to search for static substring
+ * with known length in string until the argument last. The argument n
+ * must be length of the second substring - 1.
+ */
+
+u_char *
+ngx_strlcasestrn(u_char *s1, u_char *last, u_char *s2, size_t n)
+{
+    ngx_uint_t  c1, c2;
+
+    c2 = (ngx_uint_t) *s2++;
+    c2 = (c2 >= 'A' && c2 <= 'Z') ? (c2 | 0x20) : c2;
+    last -= n;
+
+    do {
+        do {
+            if (s1 >= last) {
+                return NULL;
+            }
+
+            c1 = (ngx_uint_t) *s1++;
+
+            c1 = (c1 >= 'A' && c1 <= 'Z') ? (c1 | 0x20) : c1;
+
+        } while (c1 != c2);
+
+    } while (ngx_strncasecmp(s1, s2, n) != 0);
+
+    return --s1;
+}
+
+#define CONNECTION_CLOSE "\r\nConnection: close\r\n"
+
+static int has_connection_close(char *req, int n) {
+    return ngx_strlcasestrn(req, req + n, CONNECTION_CLOSE, sizeof(CONNECTION_CLOSE) - 2) != NULL;
+}
+
 void *handle_client(void *arg) {
     int server_fd, client_fd, epoll_fd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
     struct epoll_event ev, events[MAX_EVENTS];
-    int nfds, n, i;
+    int nfds, n, i, closing;
 
     server_fd = *(int *)arg;
 
@@ -75,10 +145,17 @@ void *handle_client(void *arg) {
                     epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
                     close(events[i].data.fd);
                 } else {
+                    // printf("got request: %.*s\n", n, buf);
+                    closing = has_connection_close(buf, n);
+                    // printf("has connection close: %d\n", closing);
                     int resp_len = snprintf(buf, sizeof(buf),
                         "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
                         sizeof(RESPONSE_BODY) - 1, RESPONSE_BODY);
                     write(events[i].data.fd, buf, resp_len);
+                    if (closing) {
+                        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+                        close(events[i].data.fd);
+                    }
                 }
             }
         }
